9d72c9caf07e9c471746da84db4c4505a584d1e5
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2021-03-29 12:33:10 -0500

    added timer and transformed data set

--- NN.py
+++ NN.py
@@ -4,6 +4,8 @@ import csv  # library used for importing CSV files
 import numpy as np
 from sklearn.neural_network import MLPClassifier  # lirbray for MLP training
 from sklearn.model_selection import cross_val_score  # library for running k-fold cross validation
+from sklearn import preprocessing # library for preprocessing the data before model training
+import timeit
 
 # Import CSV file heart_failure_clinical_records_dataset.csv
 f = open('heart_failure_clinical_records_dataset.csv', newline='')
@@ -18,21 +20,35 @@ data2d = np.array(data)
 features = data2d[:, 1: 12]  # acquire the first 12 rows as feature
 label = data2d[:, 12]  # acquire row 13 as the label
 
+# data standardization
+scaler = preprocessing.StandardScaler().fit(features)
+features_scaled = scaler.transform(features)
+
 # using the logistic activation function for logistic MLP model training
 model = MLPClassifier(activation='logistic')
 
 # run 10-fold CV for the logistic MLP model model:
 print("now computing the accuracy for the logistic MLP model: ")
+start = timeit.default_timer()
 scores = cross_val_score(model, features, label, scoring='accuracy', cv=10)
+stop = timeit.default_timer()
+print("The result of the 10-fold CV is: ")
+print(scores)
 print("The average classification accuracy for the logistic MLP model is: " + str(scores.mean()))
+print("Model training time: " + str(stop-start))
 
 # using the logistic activation function for Tanh MLP model training
 model = MLPClassifier(activation='tanh')
 
 # run 10-fold CV for the logistic MLP model model:
 print("now computing the accuracy for the Tanh MLP model: ")
+start = timeit.default_timer()
 scores = cross_val_score(model, features, label, scoring='accuracy', cv=10)
+stop = timeit.default_timer()
+print("The result of the 10-fold CV is: ")
+print(scores)
 print("The average classification accuracy for the Tanh MLP model is: " + str(scores.mean()))
+print("Model training time: " + str(stop-start))
 
 # output
 # now computing the accuracy for the logistic MLP model:
--- SVM.py
+++ SVM.py
@@ -4,6 +4,8 @@ import csv  # library used for importing CSV files
 import numpy as np
 from sklearn import svm  # library for creating SVM model
 from sklearn.model_selection import cross_val_score  # library for running k-fold cross validation
+from sklearn import preprocessing  # library for preprocessing the data before model training
+import timeit
 
 # Import CSV file heart_failure_clinical_records_dataset.csv
 f = open('heart_failure_clinical_records_dataset.csv', newline='')
@@ -18,13 +20,22 @@ data2d = np.array(data)
 features = data2d[:, 1: 12]  # acquire the first 12 rows as feature
 label = data2d[:, 12]  # acquire row 13 as the label
 
+# data standardization
+scaler = preprocessing.StandardScaler().fit(features)
+features_scaled = scaler.transform(features)
+
 # using linear kernel
 model = svm.SVC(kernel='linear')
 
 # run 10-fold CV for the linear SVC model:
 print("now computing the accuracy for the linear SVC kernel: ")
-scores = cross_val_score(model, features, label, scoring='accuracy', cv=10)
+start = timeit.default_timer()
+scores = cross_val_score(model, features_scaled, label, scoring='accuracy', cv=10)
+stop = timeit.default_timer()
+print("The result of the 10-fold CV is: ")
+print(scores)
 print("The average classification accuracy for the linear SVM model under the 10-fold CV is: " + str(scores.mean()))
+print("Model training time: " + str(stop-start))
 
 # print message
 print("")
@@ -34,8 +45,13 @@ model = svm.SVC(kernel='poly')
 
 # run 10-fold CV for the polynomial SVC model:
 print("now computing the accuracy for the polynomial SVC kernel: ")
-scores = cross_val_score(model, features, label, scoring='accuracy', cv=10)
+start = timeit.default_timer()
+scores = cross_val_score(model, features_scaled, label, scoring='accuracy', cv=10)
+stop = timeit.default_timer()
+print("The result of the 10-fold CV is: ")
+print(scores)
 print("The average classification accuracy for the polynomial SVM model under the 10-fold CV is: " + str(scores.mean()))
+print("Model training time: " + str(stop-start))
 
 # output:
 # now computing the accuracy for the linear SVC kernel:
